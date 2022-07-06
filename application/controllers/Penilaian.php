9c364b633484e0277168224fdf9c15ec449c0024
Zulmahendra <zulmahendra27@gmail.com>
2022-07-06 07:53:33 +0700

    Add features v.4

--- application/controllers/Penilaian.php
+++ application/controllers/Penilaian.php
@@ -31,56 +31,7 @@ class Penilaian extends CI_Controller
 
   public function kriteria()
   {
-    $result = $this->pk->getAllKriteria();
-
-    if (!empty($_POST)) {
-      $result = $this->kriteria->getAll();
-
-      if ($result) {
-        $urut = 0;
-        for ($x = 0; $x < (count($result) - 1); $x++) {
-          for ($y = ($x + 1); $y < count($result); $y++) {
-            $pilihanName = 'pilihan-' . $urut;
-            $nilaiName = 'nilai-' . $urut;
-
-            $pilihan = $this->input->post($pilihanName, true);
-            $nilai = $this->input->post($nilaiName, true);
-
-            if ($pilihan == 1) {
-              $bobot = round(intval($nilai), 5);
-            } else {
-              $bobot = round(1 / intval($nilai), 5);
-            }
-
-            if ($bobot == null) {
-              $bobot = 1;
-            }
-
-            $penKriteria[] = [
-              'kriteria1' => $result[$x]->id_kriteria,
-              'kriteria2' => $result[$y]->id_kriteria,
-              'nilai' => $bobot
-            ];
-
-            // $post[] = $pilihan;
-
-            $urut++;
-          }
-        }
-
-        $rs = $this->pk->insertKriteria($penKriteria);
-
-        // echo "<pre>";
-        // print_r($post);
-        // echo "</pre>";
-
-        // echo "<pre>";
-        // print_r($penKriteria);
-        // echo "</pre>";
-
-        redirect('penilaian/kriteria', 'refresh');
-      }
-    }
+    $this->setFeatures('kriteria');
 
     $data = [
       'root' => 'penilaian',
@@ -106,58 +57,9 @@ class Penilaian extends CI_Controller
       $this->load->view('page', $data);
     } else {
       $kriteria = $this->kriteria->getById($id_kriteria);
-      // $result = $this->pk->getAllAlternatif($id_kriteria);
 
       if ($kriteria) {
-        if (!empty($_POST)) {
-          $result = $this->alternatif->getAll();
-
-          if ($result) {
-            $urut = 0;
-            for ($x = 0; $x < (count($result) - 1); $x++) {
-              for ($y = ($x + 1); $y < count($result); $y++) {
-                $pilihanName = 'pilihan-' . $urut;
-                $nilaiName = 'nilai-' . $urut;
-
-                $pilihan = $this->input->post($pilihanName, true);
-                $nilai = $this->input->post($nilaiName, true);
-
-                if ($pilihan == 1) {
-                  $bobot = round(intval($nilai), 5);
-                } else {
-                  $bobot = round(1 / intval($nilai), 5);
-                }
-
-                if ($bobot == null) {
-                  $bobot = 1;
-                }
-
-                $penAlternatif[] = [
-                  'id_kriteria' => $id_kriteria,
-                  'alternatif1' => $result[$x]->id_alternatif,
-                  'alternatif2' => $result[$y]->id_alternatif,
-                  'nilai' => $bobot
-                ];
-
-                // $post[] = $pilihan;
-
-                $urut++;
-              }
-            }
-
-            $rs = $this->pk->insertAlternatif($penAlternatif, $id_kriteria);
-
-            // echo "<pre>";
-            // print_r($post);
-            // echo "</pre>";
-
-            // echo "<pre>";
-            // print_r($penKriteria);
-            // echo "</pre>";
-
-            redirect('penilaian/alternatif/' . $id_kriteria, 'refresh');
-          }
-        }
+        $this->setFeatures('alternatif', $id_kriteria);
 
         $data = [
           'root' => 'penilaian',
@@ -179,224 +81,110 @@ class Penilaian extends CI_Controller
   public function matriks($id_kriteria = null)
   {
     if ($id_kriteria == null) {
-      $result = $this->kriteria->getAll();
       $rs_nilai = $this->pk->getAllKriteria();
-      $matriks = array();
 
-      if ($result) {
-        $jmlmpb = array();
-        $jmlmnk = array();
-        $totalEigen = 0;
-
-        for ($x = 0; $x < count($result); $x++) {
-          // inisialisasi jumlah tiap kolom dan baris kriteria
-          $jmlmpb[$x] = 0;
-          $jmlmnk[$x] = 0;
-
-          for ($y = 0; $y < count($result); $y++) {
-            foreach ($rs_nilai as $rs) {
-              if ($result[$x]->id_kriteria == $rs->kriteria1 && $result[$y]->id_kriteria == $rs->kriteria2) {
-                if ($rs->nilai >= 1) {
-                  $matriks[$x][$y] = $rs->nilai;
-                  $matriks[$y][$x] = round((1 / $rs->nilai), 5);
-                } else {
-                  $matriks[$x][$y] = round($rs->nilai, 5);
-                  $matriks[$y][$x] = round((1 / $rs->nilai));
-                }
-              }
-            }
-
-            // diagonal bernilai 1
-            if ($x == $y) {
-              $matriks[$x][$y] = 1;
-            }
-
-            // menghitung jumlah pada kolom kriteria tabel perbandingan berpasangan
-            // $value = $matriks[$x][$y];
-            // $jmlmpb[$y] += $value;
-
-            // menghitung jumlah pada baris kriteria tabel nilai kriteria
-            // $matrikb[$x][$y] = $matriks[$x][$y] / $jmlmpb[$y];
-            // $value = $matrikb[$x][$y];
-            // $jmlmnk[$x] += $value;
-          }
-        }
-
-        // inisialisasi jumlah tiap kolom dan baris kriteria
-        // $jmlmpb = array();
-        // $jmlmnk = array();
-        // for ($x = 0; $x < count($result); $x++) {
-        //   $jmlmpb[$x] = 0;
-        //   $jmlmnk[$x] = 0;
-        // }
-
-        // menghitung jumlah pada kolom kriteria tabel perbandingan berpasangan
-        for ($x = 0; $x < count($result); $x++) {
-          for ($y = 0; $y < count($result); $y++) {
-            $value = $matriks[$x][$y];
-            $jmlmpb[$y] += $value;
-          }
-        }
-
-        // menghitung jumlah pada baris kriteria tabel nilai kriteria
-        // matriksb merupakan matrik yang telah dinormalisasi
-        for ($x = 0; $x < count($result); $x++) {
-          for ($y = 0; $y < count($result); $y++) {
-            $matriksb[$x][$y] = $matriks[$x][$y] / $jmlmpb[$y];
-            $value = $matriksb[$x][$y];
-            $jmlmnk[$x] += $value;
-          }
-
-          // nilai priority vektor
-          $pv[$x] = $jmlmnk[$x] / count($result);
-
-          // nilai eigen
-          $eigen[$x] = $jmlmpb[$x] * $pv[$x];
-          $totalEigen += $eigen[$x];
-        }
-
-        $ci = ($totalEigen - count($result)) / (count($result) - 1);
-        $ri = $this->pk->getRI(count($result));
-        $cr = $ci / $ri->nilai;
+      if ($rs_nilai) {
+        $data = $this->setMatriks('kriteria', $rs_nilai);
+      } else {
+        echo "<script>alert('Not Found');</script>";
+        echo "<script>window.location='" . base_url('penilaian/kriteria') . "';</script>";
       }
 
-      // echo "<pre>";
-      // print_r($cr);
-      // echo "</pre>";
-
-      $data = [
+      $data += [
         'root' => 'penilaian',
         'title' => 'Matriks Kriteria',
-        'page' => 'kriteria_matriks',
-        'result' => $result,
-        'matriks' => $matriks,
-        'normalisasi' => $matriksb,
-        'prioritas' => $pv,
-        'eigen' => $eigen,
-        'total_eigen' => $totalEigen,
-        'ci' => $ci,
-        'cr' => $cr,
-        'jmlmpb' => $jmlmpb,
-        'jmlmnk' => $jmlmnk
+        'page' => 'kriteria_matriks'
       ];
 
       $this->load->view('page', $data);
     } else {
-      $result = $this->alternatif->getAll();
       $kriteria = $this->kriteria->getById($id_kriteria);
       $rs_nilai = $this->pk->getAllAlternatif($id_kriteria);
+
       if ($kriteria && $rs_nilai) {
-        $matriks = array();
-
-        if ($result) {
-          $jmlmpb = array();
-          $jmlmnk = array();
-          $totalEigen = 0;
-
-          for ($x = 0; $x < count($result); $x++) {
-            // inisialisasi jumlah tiap kolom dan baris kriteria
-            $jmlmpb[$x] = 0;
-            $jmlmnk[$x] = 0;
-
-            for ($y = 0; $y < count($result); $y++) {
-              foreach ($rs_nilai as $rs) {
-                if ($result[$x]->id_alternatif == $rs->alternatif1 && $result[$y]->id_alternatif == $rs->alternatif2) {
-                  if ($rs->nilai >= 1) {
-                    $matriks[$x][$y] = $rs->nilai;
-                    $matriks[$y][$x] = round((1 / $rs->nilai), 5);
-                  } else {
-                    $matriks[$x][$y] = round($rs->nilai, 5);
-                    $matriks[$y][$x] = round((1 / $rs->nilai));
-                  }
-                }
-              }
-
-              // diagonal bernilai 1
-              if ($x == $y) {
-                $matriks[$x][$y] = 1;
-              }
-
-              // menghitung jumlah pada kolom kriteria tabel perbandingan berpasangan
-              // $value = $matriks[$x][$y];
-              // $jmlmpb[$y] += $value;
-
-              // menghitung jumlah pada baris kriteria tabel nilai kriteria
-              // $matrikb[$x][$y] = $matriks[$x][$y] / $jmlmpb[$y];
-              // $value = $matrikb[$x][$y];
-              // $jmlmnk[$x] += $value;
-            }
-          }
-
-          // inisialisasi jumlah tiap kolom dan baris kriteria
-          // $jmlmpb = array();
-          // $jmlmnk = array();
-          // for ($x = 0; $x < count($result); $x++) {
-          //   $jmlmpb[$x] = 0;
-          //   $jmlmnk[$x] = 0;
-          // }
-
-          // menghitung jumlah pada kolom kriteria tabel perbandingan berpasangan
-          for ($x = 0; $x < count($result); $x++) {
-            for ($y = 0; $y < count($result); $y++) {
-              $value = $matriks[$x][$y];
-              $jmlmpb[$y] += $value;
-            }
-          }
-
-          // menghitung jumlah pada baris kriteria tabel nilai kriteria
-          // matriksb merupakan matrik yang telah dinormalisasi
-          for ($x = 0; $x < count($result); $x++) {
-            for ($y = 0; $y < count($result); $y++) {
-              $matriksb[$x][$y] = $matriks[$x][$y] / $jmlmpb[$y];
-              $value = $matriksb[$x][$y];
-              $jmlmnk[$x] += $value;
-            }
-
-            // nilai priority vektor
-            $pv[$x] = $jmlmnk[$x] / count($result);
-
-            // nilai eigen
-            $eigen[$x] = $jmlmpb[$x] * $pv[$x];
-            $totalEigen += $eigen[$x];
-          }
-
-          $ci = ($totalEigen - count($result)) / (count($result) - 1);
-          $ri = $this->pk->getRI(count($result));
-          $cr = $ci / $ri->nilai;
-        }
+        $data = $this->setMatriks('alternatif', $rs_nilai);
       } else {
         echo "<script>alert('Not Found');</script>";
         echo "<script>window.location='" . base_url('penilaian/alternatif/' . $id_kriteria) . "';</script>";
       }
 
-      // echo "<pre>";
-      // print_r($cr);
-      // echo "</pre>";
-
-      $data = [
+      $data += [
         'root' => 'penilaian',
         'title' => 'Matriks Alternatif - Kriteria ' . $kriteria[0]->nama_kriteria,
-        'page' => 'alternatif_matriks',
-        'result' => $result,
-        'matriks' => $matriks,
-        'normalisasi' => $matriksb,
-        'prioritas' => $pv,
-        'eigen' => $eigen,
-        'total_eigen' => $totalEigen,
-        'ci' => $ci,
-        'cr' => $cr,
-        'jmlmpb' => $jmlmpb,
-        'jmlmnk' => $jmlmnk
+        'page' => 'alternatif_matriks'
       ];
 
       $this->load->view('page', $data);
     }
   }
 
-  public function matriksalternatif()
+  private function setFeatures($jenis, $id_kriteria = null)
   {
-    $result = $this->kriteria->getAll();
-    $rs_nilai = $this->pk->getAllAlternatif();
+    if (!empty($_POST)) {
+      if ($jenis == 'kriteria') {
+        $result = $this->kriteria->getAll();
+      } else {
+        $result = $this->alternatif->getAll();
+      }
+
+      if ($result) {
+        $urut = 0;
+        for ($x = 0; $x < (count($result) - 1); $x++) {
+          for ($y = ($x + 1); $y < count($result); $y++) {
+            $pilihanName = 'pilihan-' . $urut;
+            $nilaiName = 'nilai-' . $urut;
+
+            $pilihan = $this->input->post($pilihanName, true);
+            $nilai = $this->input->post($nilaiName, true);
+
+            if ($pilihan == 1) {
+              $bobot = round(intval($nilai), 5);
+            } else {
+              $bobot = round(1 / intval($nilai), 5);
+            }
+
+            if ($bobot == null) {
+              $bobot = 1;
+            }
+
+            if ($jenis == 'kriteria') {
+              $penilaian[] = [
+                'kriteria1' => $result[$x]->id_kriteria,
+                'kriteria2' => $result[$y]->id_kriteria,
+                'nilai' => $bobot
+              ];
+            } else {
+              $penilaian[] = [
+                'id_kriteria' => $id_kriteria,
+                'alternatif1' => $result[$x]->id_alternatif,
+                'alternatif2' => $result[$y]->id_alternatif,
+                'nilai' => $bobot
+              ];
+            }
+
+            $urut++;
+          }
+        }
+
+        if ($jenis == 'kriteria') {
+          $this->pk->insertKriteria($penilaian);
+          redirect('penilaian/matriks', 'refresh');
+        } else {
+          $this->pk->insertAlternatif($penilaian, $id_kriteria);
+          redirect('penilaian/matriks/' . $id_kriteria, 'refresh');
+        }
+      }
+    }
+  }
+
+  private function setMatriks($jenis, $rs_nilai)
+  {
+    if ($jenis == 'kriteria') {
+      $result = $this->kriteria->getAll();
+    } else {
+      $result = $this->alternatif->getAll();
+    }
+
     $matriks = array();
 
     if ($result) {
@@ -411,7 +199,19 @@ class Penilaian extends CI_Controller
 
         for ($y = 0; $y < count($result); $y++) {
           foreach ($rs_nilai as $rs) {
-            if ($result[$x]->id_kriteria == $rs->kriteria1 && $result[$y]->id_kriteria == $rs->kriteria2) {
+            if ($jenis == 'kriteria') {
+              $resultX = $result[$x]->id_kriteria;
+              $resultY = $result[$y]->id_kriteria;
+              $rs1 = $rs->kriteria1;
+              $rs2 = $rs->kriteria2;
+            } else {
+              $resultX = $result[$x]->id_alternatif;
+              $resultY = $result[$y]->id_alternatif;
+              $rs1 = $rs->alternatif1;
+              $rs2 = $rs->alternatif2;
+            }
+
+            if ($resultX == $rs1 && $resultY == $rs2) {
               if ($rs->nilai >= 1) {
                 $matriks[$x][$y] = $rs->nilai;
                 $matriks[$y][$x] = round((1 / $rs->nilai), 5);
@@ -426,25 +226,8 @@ class Penilaian extends CI_Controller
           if ($x == $y) {
             $matriks[$x][$y] = 1;
           }
-
-          // menghitung jumlah pada kolom kriteria tabel perbandingan berpasangan
-          // $value = $matriks[$x][$y];
-          // $jmlmpb[$y] += $value;
-
-          // menghitung jumlah pada baris kriteria tabel nilai kriteria
-          // $matrikb[$x][$y] = $matriks[$x][$y] / $jmlmpb[$y];
-          // $value = $matrikb[$x][$y];
-          // $jmlmnk[$x] += $value;
         }
       }
-
-      // inisialisasi jumlah tiap kolom dan baris kriteria
-      // $jmlmpb = array();
-      // $jmlmnk = array();
-      // for ($x = 0; $x < count($result); $x++) {
-      //   $jmlmpb[$x] = 0;
-      //   $jmlmnk[$x] = 0;
-      // }
 
       // menghitung jumlah pada kolom kriteria tabel perbandingan berpasangan
       for ($x = 0; $x < count($result); $x++) {
@@ -474,39 +257,21 @@ class Penilaian extends CI_Controller
       $ci = ($totalEigen - count($result)) / (count($result) - 1);
       $ri = $this->pk->getRI(count($result));
       $cr = $ci / $ri->nilai;
-    }
 
-    // echo "<pre>";
-    // print_r($cr);
-    // echo "</pre>";
+      $data = [
+        'result' => $result,
+        'matriks' => $matriks,
+        'normalisasi' => $matriksb,
+        'prioritas' => $pv,
+        'eigen' => $eigen,
+        'total_eigen' => $totalEigen,
+        'ci' => $ci,
+        'cr' => $cr,
+        'jmlmpb' => $jmlmpb,
+        'jmlmnk' => $jmlmnk
+      ];
 
-    $data = [
-      'root' => 'penilaian',
-      'title' => 'Matriks Kriteria',
-      'page' => 'kriteria_matriks',
-      'result' => $result,
-      'matriks' => $matriks,
-      'normalisasi' => $matriksb,
-      'prioritas' => $pv,
-      'eigen' => $eigen,
-      'total_eigen' => $totalEigen,
-      'ci' => $ci,
-      'cr' => $cr,
-      'jmlmpb' => $jmlmpb,
-      'jmlmnk' => $jmlmnk
-    ];
-
-    $this->load->view('page', $data);
-  }
-
-  private function bandingKriteria()
-  {
-    $result = $this->kriteria->getAll();
-
-    if (count($result) > 1) {
-      foreach ($result as $rs) {
-        # code...
-      }
+      return $data;
     }
   }
 }
